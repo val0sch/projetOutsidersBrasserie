09b90f5a9951fd944e47196048b745fb2ecf2b85
propod-op <o.pochic@gmail.com>
2022-03-23 15:43:22 +0100

    Fichiers HTML/JS/CSS ok n°1

--- assets/js/reservation.js
+++ assets/js/reservation.js
@@ -1,4 +1,16 @@
-function merci(){
-  alert('MErci de votre réservation.');
-}
+const iform = document.querySelector("#validation");
+iform.addEventListener("submit", function(event) {
+    event.preventDefault();
+    window.history.back();
+    reservationOk();
+}, true);
 
+
+function reservationOk(){
+  Swal.fire({
+    title: 'Merci !',
+    text: 'Votre réservation à été effectuée',
+    icon: 'succes',
+    confirmButtonText: 'Merci.'
+  });
+}
