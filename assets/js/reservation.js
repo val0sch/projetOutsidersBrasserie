70777e3e9e235b594c64666f9482a0b72f559477
propod-op <o.pochic@gmail.com>
2022-03-23 11:53:03 +0100

    Ajout de reservation.html

--- assets/js/reservation.js
+++ assets/js/reservation.js
@@ -1,11 +1,4 @@
-function myFunction() {
-    var x = document.getElementById("ul-nav");
-    var z = document.getElementById("nav-var");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-      navoscar.style.minHeight = "5rem"
+function merci(){
+  alert('MErci de votre réservation.');
+}
 
-    } else {
-      x.style.display = "block";
-    }
-  }
