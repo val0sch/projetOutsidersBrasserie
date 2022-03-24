466e248c2d73ab83752bfa7d270ba80e36b20823
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-03-24 21:43:26 +0100

    cght

--- assets/js/livredor.js
+++ assets/js/livredor.js
@@ -40,7 +40,16 @@ const formval = document.querySelector('.form_livredor');
 formval.addEventListener('submit', function(event) {
   event.preventDefault();
 //  myFunctionButton(event); *
-  console.log("bjr");
+  reservationOk();
+  formval.reset();
 }, true);
 
+function reservationOk(){
+  Swal.fire({
+    title: 'Merci !',
+    text: 'Votre réservation à été effectuée',
+    icon: 'succes',
+    confirmButtonText: 'Merci.'
+  });
+}
 
